2daa613476aef2419cb636fe60a4828762d4fd8d
Evan-Pei <1075247853@qq.com>
2022-06-26 14:57:18 +0800

    ssr

--- test.js
+++ test.js
@@ -17,12 +17,12 @@ const Koa = require('koa');
         //
         // ctx.body = html;
         ctx.body = `<!DOCTYPE html>
-      <html lang="en">
-        <head><title>koa2 + vite + ts + vue3 + vue-router</title></head>
-        <body>
-          <h1 style="text-align: center;">使用 koa2 + vite + ts + vue3 + vant+ router4 集成前端 SSR 企业级项目</h1>
-        </body>
-      </html>`;
+          <html lang="en">
+            <head><title>koa2 + vite + ts + vue3 + vue-router</title></head>
+            <body>
+              <h1 style="text-align: center;">使用 koa2 + vite + ts + vue3 + vant+ router4 集成前端 SSR 企业级项目</h1>
+            </body>
+          </html>`;
     });
 
     app.listen(3000, () => {
